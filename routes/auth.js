54b34065660267517535efea75004aeae5bef26b
allordiron <allordiron@gmail.com>
2016-08-17 14:44:01 +0300

    Modify auth module

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,23 @@
+// Native modules
+let crypto = require("crypto");
+// Community modules
 let express = require("express");
+// Project modules
+let db = require("../lib/db");
+let mn = require("../magicnumbers.json"); // for salt
+
+// Variables
 let router = express.Router();
 
-router.post("/", function (req, res) {
-    console.log(req.body.login);
-    console.log(req.body.password);
+router.post("/", (req, res) => {
+
+    let salt = Date.now() + mn.number;
+    console.log(salt.toString() + (req.body.password).toString());
+
+    let hash = crypto.createHash("sha512");
+    hash.update(salt.toString() + (req.body.password).toString());
+    console.log(hash.digest("hex"));
+
 
     res.redirect("/");
 });
